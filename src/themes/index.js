7bbceec7365b6a8cc2538d361e85527fd837aef6
Daniel Fallon <dan@jackboxgames.com>
2022-03-27 09:57:27 -0500

    WIP color var theme

--- src/themes/index.js
+++ src/themes/index.js
@@ -3,21 +3,32 @@ import { getThemeVariables as darkThemeVariables } from './theme-dark';
 import { getThemeVariables as defaultThemeVariables } from './theme-default';
 import { getThemeVariables as forestThemeVariables } from './theme-forest';
 import { getThemeVariables as neutralThemeVariables } from './theme-neutral';
+import { getThemeVariables as cssColorVarsThemeVariables } from './theme-csscolorvars';
 
-export default {
-  base: {
-    getThemeVariables: baseThemeVariables,
-  },
-  dark: {
-    getThemeVariables: darkThemeVariables,
-  },
-  default: {
-    getThemeVariables: defaultThemeVariables,
-  },
-  forest: {
-    getThemeVariables: forestThemeVariables,
-  },
-  neutral: {
-    getThemeVariables: neutralThemeVariables,
-  },
+const themes = {
+    base: {
+        getThemeVariables: baseThemeVariables,
+    },
+    dark: {
+        getThemeVariables: darkThemeVariables,
+    },
+    default: {
+        getThemeVariables: defaultThemeVariables,
+    },
+    forest: {
+        getThemeVariables: forestThemeVariables,
+    },
+    neutral: {
+        getThemeVariables: neutralThemeVariables,
+    },
+    csscolors: {
+        getThemeVariables: cssColorVarsThemeVariables,
+    }
 };
+
+themes.registerTheme = (name, theme) => {
+    themes[name] = theme
+}
+
+
+export default themes
